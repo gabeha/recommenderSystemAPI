e95d7914b59cb33427bac7a55fed20cdf8505089
gabeha <gabriel.hauss@gmail.com>
2023-10-18 15:53:52 +0200

    modular name, seed, domain, and zero

--- example_RecSys.py
+++ example_RecSys.py
@@ -1,15 +1,26 @@
+from helpers.cast_int_to_float import recursive_cast_to_float
 from rec_sys_uni.recommender_system import RecSys
 from rec_sys_uni.rec_systems.course_based_sys.course_based import CourseBasedRecSys
 
 def recommend_courses(student_input):
-    course_based = CourseBasedRecSys(model_name='all-MiniLM-L12-v2', # for ui
-                                    seed_help=True, # for ui
-                                    domain_adapt=True, # for ui
-                                    zero_adapt=True, # for ui
+    
+    model_name = student_input["config"]["model_name"]
+    seed_help = student_input["config"]["seed_help"]
+    domain_adapt = student_input["config"]["domain_adapt"]
+    zero_adapt = student_input["config"]["zero_adapt"]
+
+    print(model_name, seed_help, domain_adapt, zero_adapt)
+
+    course_based = CourseBasedRecSys(model_name=model_name,
+                                    seed_help=seed_help,
+                                    domain_adapt=domain_adapt,
+                                    zero_adapt=zero_adapt,
                                     domain_type='title',
                                     seed_type='title',
                                     zero_type='title',
                                     precomputed_course=True)
+    
+    student_input = recursive_cast_to_float(student_input)
 
 
 
@@ -22,7 +33,7 @@ def recommend_courses(student_input):
         results = rs.get_recommendation(student_input)
         output = {'structured_recommendation': results.get('structured_recommendation'),
         'explanation': results.get('explanation'), "student_input": student_input}
-        print (output)
+        # print (output)
     except Exception as e:
         output = {'error': str(e)}
     finally:
