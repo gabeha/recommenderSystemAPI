ff0cd114b42cee903c17824252d80476e7b501aa
DQBO1998 <50704553+DQBO1998@users.noreply.github.com>
2023-12-02 18:42:35 +0100

    Integrated ILP planner.

--- app.py
+++ app.py
@@ -69,15 +69,8 @@ def get_explanation():
 def get_timeline():
     input = request.get_json()
     student_mongo_id = input['student_mongo_id']
-
-    # Request the recommended courses and their weights from the database
-    # recommended_courses = request_to_mongo_db()
-    
-    # Call the timeline generator
-    # timeline = rs.generate_timeline(student_mongo_id, course_code)
-
-    # return the timeline
-    # return jsonify({'timeline': timeline})
+    timeline = rs.make_timeline(student_mongo_id)
+    return jsonify({'timeline': timeline})
 
 
 if __name__ == '__main__':
